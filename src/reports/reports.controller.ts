4083e4fdc4735a0dce31e4da10df328d3d5f90de
Anthony van Voorst <anthony_2v@hotmail.com>
2023-04-13 10:36:45 -0400

    Mapped out future routes with comments

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+    /**
+     * Report how much a vehicle sold for
+     */
+
+    /**
+     * Get an estimate for the car's value
+     */
+
+    /**
+     * Approve or reject a report submitted by a user
+     */
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    /**
+     * Create a new user and sign in
+     */
+    @Post('/signup')
+    createUser(@Body() body: { email: string, password: string }) {}
+
+    /**
+     * Sign in as an existing user
+     */
+    @Post('/signin')
+    login(@Body() body: { email: string, password: string }) {}
+
+    /**
+     * Find a user with a given id
+     * NOTE: Not needed for the production app, it is for understanding TypeORM
+     */
+
+    /**
+     * Find all users with the given email
+     * NOTE: Not needed for the production app, it is for understanding TypeORM
+     */
+
+    /**
+     * Update a user with the given id
+     * NOTE: Not needed for the production app, it is for understanding TypeORM
+     */
+
+    /**
+     * Delete a user with the given id
+     * NOTE: Not needed for the production app, it is for understanding TypeORM
+     */
+}
