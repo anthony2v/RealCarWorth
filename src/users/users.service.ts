b91fd22be5f7c1df2f2dbe4b92a48524ea89c9a8
Anthony van Voorst <anthony_2v@hotmail.com>
2023-04-27 09:41:02 -0400

    Added update and remove methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
@@ -9,8 +9,8 @@ export class UsersService {
 
     public create(email: string, password: string): Promise<UserEntity> {
         // we create a UserEntity instance here to facilitate typeorm logging hooks
-        const user: UserEntity = this.repository.create({ email, password });
-        return this.repository.save(user);
+        const userEntity: UserEntity = this.repository.create({ email, password });
+        return this.repository.save(userEntity);
     }
 
     public find(email: string): Promise<UserEntity[]> {
@@ -19,5 +19,22 @@ export class UsersService {
 
     public findOne(id: number): Promise<UserEntity | null> {
         return this.repository.findOneBy({ id });
+    }
+
+    public async update(id: number, attributes: Partial<UserEntity>): Promise<UserEntity> {
+        const userEntity: UserEntity | null = await this.findOne(id);
+        if (!userEntity)
+            throw new BadRequestException("User not found");
+
+        Object.assign(userEntity, attributes);
+        return this.repository.save(userEntity);
+    }
+
+    public async remove(id: number): Promise<UserEntity> {
+        const userEntity: UserEntity | null = await this.findOne(id);
+        if (!userEntity)
+            throw new BadRequestException("User not found");
+            
+        return this.repository.remove(userEntity);
     }
 }
