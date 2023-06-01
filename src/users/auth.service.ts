c3d94b6bc247e75507d12607669fb4345c0fa917
Anthony van Voorst <anthony_2v@hotmail.com>
2023-06-01 09:21:44 -0400

    Started implementing the createAccount method

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
+
+  async createAccount(email: string, password: string): Promise<string> {
+    // see if email is in use
+    if ((await this.usersService.find(email)).length) throw new BadRequestException('Email in use');
+
+    // hash the user's password
+
+    // create a new user and save it
+
+    // return the user id
+    return '';
+  }
+
+  authenticate() {}
 }
